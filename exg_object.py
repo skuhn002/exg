5a53be40c52f39bb4f0b16b0877e829d9d2d5cc4
skuhn002 <kuhnsamuel2@gmail.com>
2021-07-18 12:57:26 -0400

    Make File sub-classes of the exg object
    File subclasses will hold information specific to a certain file that has been loaded. Currently only holds filename.

--- exg_object.py
+++ exg_object.py
@@ -5,20 +5,30 @@ class exg():
     def __init__(self):
         root = tkinter.Tk()
         root.withdraw()
-        self.files = filedialog.askopenfiles(parent=root, mode='r',
+        self.raw_files = filedialog.askopenfiles(parent=root, mode='r',
                                             filetypes=[("All files", "*"),
                                                         ("BYB files","BYB_Recording*.wav"),
                                                         ("OpenBCI files", ["OpenBCI*.txt", "OpenBCI*.bdf"]),
                                                         ("Excel file","*.xls")],
                                              title='Choose an excel file')
-        if len(self.files) > 0:
+        if len(self.raw_files) > 0:
             print("File(s): ")
-            for i in range(len(self.files)):
-                print("\t" + self.files[0].name)
+            for i in range(len(self.raw_files)):
+                print("\t" + self.raw_files[0].name)
+
+    def prep_files(self):
+        self.files = []
+        junk = "Hi, I'm junk"
+        for i in range(len(self.raw_files)):
+            print('File ' + str(i+1) + ': \n\t' + self.raw_files[i].name)
+            filename = self.raw_files[i].name
+            this_file = {'filename': filename}
+            self.files.append(this_file)
+
 
 
 EXG = exg()
-if len(EXG.files) > 0:
+if len(EXG.raw_files) > 0:
     print("\nEXG object named 'EXG' created")
 else:
     print("\nEXG object named 'EXG' created without any data files")
