6e7b1ccc9d3cec9757c30598c171b2b5a75428cb
skuhn002 <kuhnsamuel2@gmail.com>
2021-07-18 13:28:06 -0400

    Add get_data()
    Add a method to get the data for a specific file. One channel BYB file first.

--- exg_object.py
+++ exg_object.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import filedialog
+from scipy.io import wavfile
 
 class exg():
     def __init__(self):
@@ -16,14 +17,25 @@ class exg():
             for i in range(len(self.raw_files)):
                 print("\t" + self.raw_files[0].name)
 
+    def get_data(self, filepath, format='BYB', debug=False):
+        if format == 'BYB':
+            if 'BYB_Recording' not in filepath:
+                print('This may not be in BYB format')
+            sample_rate, Y = wavfile.read(filepath)
+            if debug:
+                print('Sample Rate: ' + str(sample_rate))
+                print("First 10 Samples: " + str(Y[0:10]))
+            return sample_rate, Y
+
     def prep_files(self):
         self.files = []
-        junk = "Hi, I'm junk"
         for i in range(len(self.raw_files)):
             print('File ' + str(i+1) + ': \n\t' + self.raw_files[i].name)
             filename = self.raw_files[i].name
-            this_file = {'filename': filename}
+            sample_rate, data = self.get_data(filename, format='BYB', debug=True)
+            this_file = {'filename': filename, 'sample_rate': sample_rate, 'data': data}
             self.files.append(this_file)
+
 
 
 
@@ -32,3 +44,5 @@ if len(EXG.raw_files) > 0:
     print("\nEXG object named 'EXG' created")
 else:
     print("\nEXG object named 'EXG' created without any data files")
+
+EXG.prep_files()
